63c2d40f1751cdbfda12b5681d70ffb931b97fc3
Tori <torimpham@gmail.com>
2020-02-22 13:19:04 -0800

    added env file to protect mysql login

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const Employee = require("./class");
 
 
 const connection = new Database({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "poop",
-    database: "employee_trackerDB"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
   });
 
 
